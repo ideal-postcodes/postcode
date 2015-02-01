be7a1d8323ba6354d6393352acea4f4ac2e202a1
James Billingham <james@billingham.net>
2015-02-01 21:49:46 +0000

    Added support for areas, sectors & units
    Based on: http://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Formatting

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 
 var validationRegex = /^[a-z0-9]{1,4}\s*?\d[a-z]{2}$/i,
 		incodeRegex = /\d[a-z]{2}$/i,
-		validOutcodeRegex = /^[a-z0-9]{1,4}$/i;
+		validOutcodeRegex = /^[a-z0-9]{1,4}$/i,
+		areaRegex = /^[a-z]{1,2}/i,
+		sectorRegex = /^[a-z0-9]{1,4}\s*?\d/i,
+		unitRegex = /[a-z]{2}$/i;
 
 function isValidPostcode (postcode) {
 	return !!postcode.match(validationRegex);
@@ -14,6 +17,18 @@ function parseOutcode (postcode) {
 
 function parseIncode (postcode) {
 	return postcode.match(incodeRegex)[0];
+}
+
+function parseArea (postcode) {
+	return postcode.match(areaRegex)[0];
+}
+
+function parseSector (postcode) {
+	return postcode.match(sectorRegex)[0];
+}
+
+function parseUnit (postcode) {
+	return postcode.match(unitRegex)[0];
 }
 
 function Postcode (rawPostcode) {
@@ -45,6 +60,27 @@ Postcode.prototype.outcode = function () {
 	if (this._outcode) return this._outcode;
 	this._outcode = parseOutcode(this._raw).toUpperCase();
 	return this._outcode;
+}
+
+Postcode.prototype.area = function () {
+	if (!this._valid) return null;
+	if (this._area) return this._area;
+	this._area = parseArea(this._raw).toUpperCase();
+	return this._area;
+}
+
+Postcode.prototype.sector = function () {
+	if (!this._valid) return null;
+	if (this._sector) return this._sector;
+	this._sector = parseSector(this.normalise()).toUpperCase();
+	return this._sector;
+}
+
+Postcode.prototype.unit = function () {
+	if (!this._valid) return null;
+	if (this._unit) return this._unit;
+	this._unit = parseUnit(this._raw).toUpperCase();
+	return this._unit;
 }
 
 Postcode.prototype.normalise = function () {
