37a16f2dec137e61092698a7b9bc9ead62fb8ada
Christopher Blanchard <git@nf3.co>
2021-02-02 18:56:38 +0000

    test(Postcodes): Pull latest data from CDN

--- test/exhaustive_unit.ts
+++ test/exhaustive_unit.ts
@@ -1,33 +1,27 @@
 /* tslint:disable:no-invalid-this */
 
 import { assert } from "chai";
-import { createReadStream } from "fs";
-import csvParse from "csv-parse";
-import { resolve } from "path";
-import { createGunzip } from "zlib";
+import axios from "axios";
 
 const TIMEOUT = 60000;
-const PC_LENGTH = 7;
 
 import { parse } from "../lib/index";
 
-type CsvRecord = [string];
+const url = "https://data.ideal-postcodes.co.uk/postcodes.csv";
 
 describe("Exhaustive postcode test", () => {
-  const postcodes: string[] = [];
+  let postcodes: string[] = [];
 
-  before(function (done) {
+  before(async function () {
     this.timeout(TIMEOUT);
     console.log(
-      "Loading in complete array of postcodes (this might take a while)..."
+      "Retrieving latest postcode dataset from https://data.ideal-postcodes.co.uk"
     );
-    const inputFile = resolve(__dirname, "./data/postcodes.csv.gz");
-    createReadStream(inputFile)
-      .pipe(createGunzip())
-      .pipe(csvParse({ delimiter: "," }))
-      .on("data", (data: CsvRecord) => postcodes.push(data[0]))
-      .on("error", done)
-      .on("end", done);
+    const response = await axios({ url });
+    postcodes = response.data
+      .split(/\r\n|\r|\n/)
+      .map((p: string) => p.trim())
+      .filter((p: string) => p !== "GIR 0AA");
   });
 
   describe(".valid", () => {
@@ -47,16 +41,9 @@ describe("Exhaustive postcode test", () => {
         const p = parse(postcode);
         const downcasePostcode = parse(postcode.toLowerCase());
         const unspacedPostcode = parse(postcode.replace(/\s/, ""));
-        if (postcode.length === PC_LENGTH) {
-          // Since this isn't normalised in dataset, best we can do is see if normalised data matches
-          assert.equal(p.postcode, downcasePostcode.postcode);
-          assert.equal(p.postcode, unspacedPostcode.postcode);
-        } else {
-          // Any space indicates incode/outcode
-          assert.equal(p.postcode, postcode.replace(/\s*/, " "));
-          assert.equal(downcasePostcode.postcode, postcode.replace(/\s*/, " "));
-          assert.equal(unspacedPostcode.postcode, postcode.replace(/\s*/, " "));
-        }
+        // Since this isn't normalised in dataset, best we can do is see if normalised data matches
+        assert.equal(p.postcode, downcasePostcode.postcode);
+        assert.equal(p.postcode, unspacedPostcode.postcode);
       });
     });
   });
@@ -68,20 +55,9 @@ describe("Exhaustive postcode test", () => {
         const p = parse(postcode);
         const downcasePostcode = parse(postcode.toLowerCase());
         const unspacedPostcode = parse(postcode.replace(/\s/, ""));
-
-        if (postcode.length === PC_LENGTH) {
-          // Since this isn't normalised in dataset, best we can do is see if normalised data matches
-          assert.equal(p.incode, downcasePostcode.incode);
-          assert.equal(p.incode, unspacedPostcode.incode);
-        } else {
-          // Any space indicates incode/outcode
-          const match = postcode.match(/.*\s/);
-          if (match === null) throw new Error("null detected");
-          const testIncode = match[0].replace(/\s/, "");
-          assert.equal(p.incode, testIncode);
-          assert.equal(downcasePostcode.incode, testIncode);
-          assert.equal(unspacedPostcode.incode, testIncode);
-        }
+        // Since this isn't normalised in dataset, best we can do is see if normalised data matches
+        assert.equal(p.incode, downcasePostcode.incode);
+        assert.equal(p.incode, unspacedPostcode.incode);
       });
     });
   });
@@ -93,19 +69,9 @@ describe("Exhaustive postcode test", () => {
         const p = parse(postcode);
         const downcasePostcode = parse(postcode.toLowerCase());
         const unspacedPostcode = parse(postcode.replace(/\s/, ""));
-        if (postcode.length === PC_LENGTH) {
-          // Since this isn't normalised in dataset, best we can do is see if normalised data matches
-          assert.equal(p.outcode, downcasePostcode.outcode);
-          assert.equal(p.outcode, unspacedPostcode.outcode);
-        } else {
-          // Any space indicates incode/outcode
-          const match = postcode.match(/\s.*/);
-          if (match === null) throw new Error("null detected");
-          const testIncode = match[0].replace(/\s/, "");
-          assert.equal(p.outcode, testIncode);
-          assert.equal(downcasePostcode.outcode, testIncode);
-          assert.equal(unspacedPostcode.outcode, testIncode);
-        }
+        // Since this isn't normalised in dataset, best we can do is see if normalised data matches
+        assert.equal(p.outcode, downcasePostcode.outcode);
+        assert.equal(p.outcode, unspacedPostcode.outcode);
       });
     });
   });
@@ -116,19 +82,9 @@ describe("Exhaustive postcode test", () => {
         const p = parse(postcode);
         const downcasePostcode = parse(postcode.toLowerCase());
         const unspacedPostcode = parse(postcode.replace(/\s/, ""));
-        if (postcode.length === PC_LENGTH) {
-          // Since this isn't normalised in dataset, best we can do is see if normalised data matches
-          assert.equal(p.area, downcasePostcode.area);
-          assert.equal(p.area, unspacedPostcode.area);
-        } else {
-          // Any space indicates incode/outcode
-          const match = postcode.match(/\s.*/);
-          if (match === null) throw new Error("null detected");
-          const testArea = match[0].replace(/\s/, "");
-          assert.equal(p.area, testArea);
-          assert.equal(downcasePostcode.area, testArea);
-          assert.equal(unspacedPostcode.area, testArea);
-        }
+        // Since this isn't normalised in dataset, best we can do is see if normalised data matches
+        assert.equal(p.area, downcasePostcode.area);
+        assert.equal(p.area, unspacedPostcode.area);
       });
     });
   });
@@ -139,19 +95,9 @@ describe("Exhaustive postcode test", () => {
         const p = parse(postcode);
         const downcasePostcode = parse(postcode.toLowerCase());
         const unspacedPostcode = parse(postcode.replace(/\s/, ""));
-        if (postcode.length === PC_LENGTH) {
-          // Since this isn't normalised in dataset, best we can do is see if normalised data matches
-          assert.equal(p.district, downcasePostcode.district);
-          assert.equal(p.district, unspacedPostcode.district);
-        } else {
-          // Any space indicates incode/outcode
-          const match = postcode.match(/\s.*/);
-          if (match === null) throw new Error("null detected");
-          const testDistrict = match[0].replace(/\s/, "");
-          assert.equal(p.district, testDistrict);
-          assert.equal(downcasePostcode.district, testDistrict);
-          assert.equal(unspacedPostcode.district, testDistrict);
-        }
+        // Since this isn't normalised in dataset, best we can do is see if normalised data matches
+        assert.equal(p.district, downcasePostcode.district);
+        assert.equal(p.district, unspacedPostcode.district);
       });
     });
   });
@@ -162,19 +108,9 @@ describe("Exhaustive postcode test", () => {
         const p = parse(postcode);
         const downcasePostcode = parse(postcode.toLowerCase());
         const unspacedPostcode = parse(postcode.replace(/\s/, ""));
-        if (postcode.length === PC_LENGTH) {
-          // Since this isn't normalised in dataset, best we can do is see if normalised data matches
-          assert.equal(p.subDistrict, downcasePostcode.subDistrict);
-          assert.equal(p.subDistrict, unspacedPostcode.subDistrict);
-        } else {
-          // Any space indicates incode/outcode
-          const match = postcode.match(/\s.*/);
-          if (match === null) throw new Error("null detected");
-          const testSubDistrict = match[0].replace(/\s/, "");
-          assert.equal(p.subDistrict, testSubDistrict);
-          assert.equal(downcasePostcode.subDistrict, testSubDistrict);
-          assert.equal(unspacedPostcode.subDistrict, testSubDistrict);
-        }
+        // Since this isn't normalised in dataset, best we can do is see if normalised data matches
+        assert.equal(p.subDistrict, downcasePostcode.subDistrict);
+        assert.equal(p.subDistrict, unspacedPostcode.subDistrict);
       });
     });
   });
@@ -185,19 +121,8 @@ describe("Exhaustive postcode test", () => {
         const p = parse(postcode);
         const downcasePostcode = parse(postcode.toLowerCase());
         const unspacedPostcode = parse(postcode.replace(/\s/, ""));
-        if (postcode.length === PC_LENGTH) {
-          // Since this isn't normalised in dataset, best we can do is see if normalised data matches
-          assert.equal(p.sector, downcasePostcode.sector);
-          assert.equal(p.sector, unspacedPostcode.sector);
-        } else {
-          // Any space indicates incode/outcode
-          const match = postcode.match(/\s.*/);
-          if (match === null) throw new Error("null detected");
-          const testSector = match[0].replace(/\s/, "");
-          assert.equal(p.sector, testSector);
-          assert.equal(downcasePostcode.sector, testSector);
-          assert.equal(unspacedPostcode.sector, testSector);
-        }
+        assert.equal(p.sector, downcasePostcode.sector);
+        assert.equal(p.sector, unspacedPostcode.sector);
       });
     });
   });
@@ -208,19 +133,9 @@ describe("Exhaustive postcode test", () => {
         const p = parse(postcode);
         const downcasePostcode = parse(postcode.toLowerCase());
         const unspacedPostcode = parse(postcode.replace(/\s/, ""));
-        if (postcode.length === PC_LENGTH) {
-          // Since this isn't normalised in dataset, best we can do is see if normalised data matches
-          assert.equal(p.unit, downcasePostcode.unit);
-          assert.equal(p.unit, unspacedPostcode.unit);
-        } else {
-          // Any space indicates incode/outcode
-          const match = postcode.match(/\s.*/);
-          if (match === null) throw new Error("null detected");
-          const testUnit = match[0].replace(/\s/, "");
-          assert.equal(p.unit, testUnit);
-          assert.equal(downcasePostcode.unit, testUnit);
-          assert.equal(unspacedPostcode.unit, testUnit);
-        }
+        // Since this isn't normalised in dataset, best we can do is see if normalised data matches
+        assert.equal(p.unit, downcasePostcode.unit);
+        assert.equal(p.unit, unspacedPostcode.unit);
       });
     });
   });
